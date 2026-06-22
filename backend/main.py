7d67277df3870bb63b2aec99e1a0e706c8141884
ilunga mpoyo ben <mpoyoben61agmail.com>
2026-06-22 11:13:07 +0200

    trigger deploy

--- backend/main.py
+++ backend/main.py
@@ -495,3 +495,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     import os
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
+# deploy trigger 2026-06-22 11:13:06
